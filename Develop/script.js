2f834d1a8f550b160171b5e8fe87063dacfc0133
iburley33 <iburley33@gmail.com>
2023-01-07 17:21:36 -0500

    updated readme

--- Develop/script.js
+++ Develop/script.js
@@ -31,7 +31,6 @@ function promptAll() {
   var incSymbol = window.confirm ("Would you like to include symbols?")
   return {incLower, incUpper, incNum, incSymbol}
 }
-
 function generatePassword() {
 var passwordValues = [];  
   for (let i = 0; i < masterLength; i++) {
@@ -40,7 +39,6 @@ var passwordValues = [];
     passwordValues.push(randomVal)
   } return passwordValues.join('')
 }
-
 function getRandomInd(value) {
 var index = Math.floor(Math.random() * value)
 return index
@@ -79,18 +77,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-/* WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria */
-
-
-/* WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password */
-/* 
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters */
-/* WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected */
-/* WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria */
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Password-Generator
+ 
+## Description
+Technology used
+1. html
+2. css
+3. js
+
+## Purpose
+Random Password Generator that accepts 
+
+## Challenges 
+Through this project I started over about 3 or 4 times as I continued to hit the same wall and tried to give myself new baselines to work with before hitting the wall.
+
+The wall in question was adding the contents of the individual arrays into one master array. My tutor assisted me by reintroducing .push and gave me the strategy of creating a base blank array. With this information it became pretty logical as I already had the vision of using the for loop on my new array but simply couldnt figure out how to compile them into 1 based on the selected prompts. 
+
+Despite the frustration associated I learned a ton from this and got a lot of practice in. In the end, once it was complete, I loved this project!
+
+
+
+
+
